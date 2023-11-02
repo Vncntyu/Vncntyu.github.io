033155250c7ed193ffbe9d868cbdf8a40fa66d3b
Vncntyu <145072550+Vncntyu@users.noreply.github.com>
2023-11-02 17:36:08 -0400

    java button

---
+++ src/javascripts/cool-button.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  const nameButton = document.createElement('button');
+  nameButton.textContent = 'Click Me';
+
+  nameButton.addEventListener('click', () => {
+    // Generate a random color
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+    // Change the background color of the header
+    header.style.backgroundColor = randomColor;
+  });
+
+  header.appendChild(nameButton);
+});
